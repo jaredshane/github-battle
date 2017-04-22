b3da91a3474555f97d962de954825666e3d55403
Jared Fuller <jaredshane@gmail.com>
2017-04-22 10:06:21 -0500

    created a select language component to break up the Popular component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,30 @@
 const React = require('react')
+const PropTypes = require('prop-types')
 
+
+//stateless functional component
+let SelectLanguage = (props) => {
+  let languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
+  return (
+    <ul className='languages'>
+      {languages.map(function(lang) {
+        return (
+          <li
+            style={lang === props.selectedLanguage ? { color: '#d0021b'} : null}
+            onClick={props.onSelect.bind(null, lang)}
+            key={lang}>
+            {lang}
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired
+}
 
 class Popular extends React.Component {
   constructor (props) {
