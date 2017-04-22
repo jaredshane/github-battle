72db762aebb6cd33aba7e886efc2bc767749b7d8
Jared Fuller <jaredshane@gmail.com>
2017-04-22 11:06:39 -0500

    added a ternary to add a loading screen if this.state.repos is null if not null, then will display the RepoGrid component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -91,6 +91,8 @@ class Popular extends React.Component {
           selectedLanguage={this.state.selectedLanguage}
           onSelect={this.updateLanguage}
         />
+        {!this.state.repos ? <p>LOADING</p> :   <RepoGrid repos={this.state.repos} />}
+
       </div>
     )
   }
