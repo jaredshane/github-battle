40bed07d305c8982ef808ed69510c71fd0c25535
Jared Fuller <jaredshane@gmail.com>
2017-04-22 13:11:56 -0500

    set up the playerPreview render

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,26 @@
 const React = require('react')
 const PropTypes = require('prop-types')
+const { Link } = require('react-router-dom')
+
+let PlayerPreview = (props) => {
+  return (
+    <div>
+      <div className='column'>
+        <img className='avatar' src={props.avatar} alt={'Avatar for ' + props.username} />
+        <h2 className='username'> @{props.username}</h2>
+      </div>
+      <button className='reset' onClick={props.onReset.bind(null, props.id)}> Reset </button>
+    </div>
+
+  )
+}
+
+PlayerPreview.proptypes = {
+  avatar: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  onReset: PropTypes.func.isRequired
+}
 
 class PlayerInput extends React.Component {
   constructor(props) {
