249d9f80f12911624875bd06f7d4594e4f48ee38
Jared Fuller <jaredshane@gmail.com>
2017-04-22 11:05:22 -0500

    set the RepoGrid component to take the data from the ajax request and populate a list of popular github repos with it based on the language selected

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -27,22 +27,62 @@ SelectLanguage.propTypes = {
   onSelect: PropTypes.func.isRequired
 }
 
+let RepoGrid = (props) => {
+  return (
+    <ul className='popular-list'>
+      {props.repos.map( (repo, index) => {
+        return (
+          <li key={repo.name} className='popular-item'>
+            <div className='popular-rank'>#{index + 1}</div>
+            <ul className='space-list-items'>
+              <li>
+                <img className='avatar' src={repo.owner.avatar_url} alt={'Avatar for ' + repo.owner.login} />
+              </li>
+              <li><a href={repo.html_url}>{repo.name}</a></li>
+              <li>@{repo.owner.login}</li>
+              <li>{repo.stargazers_count} stars</li>
+            </ul>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+RepoGrid.propTypes = {
+  repos: PropTypes.array.isRequired
+}
+
 class Popular extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
       selectedLanguage: 'All',
+      repos: null
     }
 
     this.updateLanguage = this.updateLanguage.bind(this)
   }
 
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage)
+  }
+
   updateLanguage(lang) {
     this.setState(function () {
       return {
-        selectedLanguage: lang
+        selectedLanguage: lang,
+        repos: null
       }
     })
+    api.fetchPopularRepos(lang)
+      .then( (repos) => {
+        this.setState( () => {
+          return {
+            repos: repos
+          }
+        })
+      })
   }
   render() {
     return (
