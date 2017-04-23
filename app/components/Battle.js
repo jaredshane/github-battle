a4961cd51f2aa370e9fbfb6c6e1900b00339d871
Jared Fuller <jaredshane@gmail.com>
2017-04-23 16:28:39 -0500

    refactored player 2 for PlayerPreview as well

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -120,10 +120,9 @@ class Battle extends React.Component {
           {playerTwoImage !== null  &&
             <PlayerPreview
               avatar={playerTwoImage}
-              username={playerTwoName}
-              onReset={this.handleReset}
-              id='playerTwo'
-            />}
+              username={playerTwoName}>
+              <button className='reset' onClick={this.handleReset.bind(null, 'playerTwo')}> Reset </button>
+            </PlayerPreview>}
         </div>
         {playerOneImage && playerTwoImage &&
           <Link
