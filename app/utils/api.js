1635c1359bcdd55fc17aaf564898fcf85ba585e2
Jared Fuller <jaredshane@gmail.com>
2017-04-23 14:53:17 -0500

    added some functions to make an api call to get repos profile data for a user and handle that data to get star count and calculate the score for the github battle

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,4 +1,6 @@
 const axios = require('axios')
+
+
 module.exports.fetchPopularRepos = (language) => {
     let encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories')
 
@@ -7,3 +9,59 @@ module.exports.fetchPopularRepos = (language) => {
         return res.data.items
       })
   }
+
+let getProfile = (username) => {
+  return axios.get(`https://api.github.com/users/${username}`)
+    .then( (user) => {
+      return user.data
+    })
+}
+
+let getRepos = (username) => {
+  return axios.get(`https://api.github.com/users/${username}/repos&per_page=100`)
+}
+
+let getStarCount = (repos) => {
+  return repos.data.reduce( (count, repo) => {
+    return count + repo.stargazers_count
+  }, 0)
+}
+
+let calculateScore = (profile, repos) => {
+  let followers = profile.followers
+  let totalStars = getStarCount(repos)
+
+  return (followers * 3) + totalStars
+}
+
+let handleError = (error) => {
+  console.warn(error)
+  return null
+}
+
+let getUserData = (player) => {
+  return axios.all([
+      getProfile(player),
+      getRepos(player)
+  ]).then((data) => {
+    let profile = data[0]
+    let repos = data[1]
+    return {
+      profile: profile,
+      score: calculateScore(profile, repos)
+    }
+  })
+}
+
+let sortPlayers = (players) => {
+  return players.sort( (a,b) => {
+    return b.score - a.score
+  })
+}
+
+module.exports.battle = (players) => {
+  return axios.all(players.map(getUserData))
+    .then(sortPlayers)
+    .catch(handleError)
+}
+}
